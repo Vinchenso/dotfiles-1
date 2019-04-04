1e0f9bd6b6780d0224b7f86b3798c9043e001d11
Aria Edmonds <aria@ar1as.space>
2019-04-05 09:03:19 +1100

    sort system.nix

--- nixos/system.nix
+++ nixos/system.nix
@@ -1,50 +1,55 @@
 {config, pkgs, ...}:
 
 {
-    environment.systemPackages = with pkgs; [
-      wget vim git (ripgrep.override {withPCRE2 = true;}) fd stow exa nix-index
-      gnupg fzf atool file tmux pv binutils
-    ];
+  boot.cleanTmpDir = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
 
-  nix.trustedUsers = [ "root" "aria" ];
+  environment.etc.current-nixos-config.source = ./.;
+  environment.systemPackages = with pkgs; [
+    wget vim git (ripgrep.override {withPCRE2 = true;}) fd stow exa nix-index
+    gnupg fzf atool file tmux pv binutils
+  ];
 
+  fonts.fontconfig.defaultFonts.monospace = [ "Iosevka" ];
   fonts.fonts = with pkgs; [
     fira-code-symbols noto-fonts symbola noto-fonts-cjk font-awesome_5
   ];
-  fonts.fontconfig.defaultFonts.monospace = [ "Iosevka" ];
 
-  programs.mtr.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  networking.extraHosts = builtins.readFile (builtins.fetchurl { name = "blocked_hosts.txt"; url = "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"; });
+  networking.networkmanager.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  nix.trustedUsers = [ "root" "aria" ];
+
+  nixpkgs.config.allowUnfree = true;
+
   programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  programs.mosh.enable = true;
+  programs.mtr.enable = true;
+
   programs.zsh.enable = true;
   programs.zsh.promptInit = "";
-  programs.mosh.enable = true;
-  networking.networkmanager.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  services.avahi.enable = true;
+  services.avahi.ipv6 = true;
+  services.avahi.nssmdns = true;
+  services.avahi.publish.addresses = true;
+  services.avahi.publish.domain = true;
+  services.avahi.publish.enable = true;
+
+  services.compton.enable = true;
+
+  services.locate = {
+    enable = true;
+    locate = pkgs.mlocate;
+    interval = "hourly";
+  };
 
   services.openssh.enable = true;
   services.openssh.startWhenNeeded = true;
-
-  services.unbound.enable = true;
-
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-  services.avahi.ipv6 = true;
-  services.avahi.publish.enable = true;
-  services.avahi.publish.addresses = true;
-  services.avahi.publish.domain = true;
-
-  time.timeZone = "Australia/Sydney";
-
-  services.xserver.autoRepeatDelay = 200;
-  services.xserver.autoRepeatInterval = 25;
-
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.windowManager.bspwm.enable = true;
-  services.xserver.xkbOptions = "compose:ralt";
-  services.compton.enable = true;
 
   services.syncthing = {
     enable = true;
@@ -53,15 +58,18 @@
     dataDir = "/home/aria/.config/syncthing";
   };
 
-  networking.extraHosts = builtins.readFile (builtins.fetchurl { name = "blocked_hosts.txt"; url = "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"; });
+  services.unbound.enable = true;
 
-  environment.etc.current-nixos-config.source = ./.;
+  services.xserver.autoRepeatDelay = 200;
+  services.xserver.autoRepeatInterval = 25;
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.windowManager.bspwm.enable = true;
+  services.xserver.xkbOptions = "compose:ralt";
 
-  services.locate = {
-    enable = true;
-    locate = pkgs.mlocate;
-    interval = "hourly";
-  };
+  sound.enable = true;
+
+  time.timeZone = "Australia/Sydney";
 
   users.users.aria = {
     isNormalUser = true;
@@ -69,13 +77,4 @@
     shell = pkgs.zsh;
     extraGroups = [ "wheel" "networkmanager" "video" "mlocate" ];
   };
-
-  nixpkgs.config.allowUnfree = true;
-
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.cleanTmpDir = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
 }
