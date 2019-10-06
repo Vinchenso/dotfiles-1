cf898a65aa4852fde3bafda83955e55833910616
Aria Edmonds <aria@ar1as.space>
2019-10-06 16:37:55 +1100

    install pwgen

--- nixos/home.nix
+++ nixos/home.nix
@@ -3,10 +3,10 @@
 {
   home.packages = with pkgs; [
     htop qutebrowser termite networkmanagerapplet dunst libnotify mpv pass
-    rofi-pass discord gimp tldr bat pavucontrol xclip gibo spotify nmap binutils
-    units ffmpeg tetex okular maim xdotool xsel libxml2 gitAndTools.hub clang
-    virtmanager barrier qbittorrent ranger weechat looking-glass-client pb_cli sshfs
-    nodejs-10_x yarn direnv html-tidy
+    rofi-pass pwgen discord gimp tldr bat pavucontrol xclip gibo spotify nmap
+    binutils units ffmpeg tetex okular maim xdotool xsel libxml2 gitAndTools.hub
+    clang virtmanager barrier qbittorrent ranger weechat looking-glass-client
+    pb_cli sshfs nodejs-10_x yarn direnv html-tidy
 
     # to install argon2
     python gnumake
