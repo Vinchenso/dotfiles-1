525b5fe1364e9a5791cecbb72bea70eb39c725db
Aria Edmonds <aria@ar1as.space>
2019-11-27 02:43:49 +1100

    make font smaller, disable big-font-mode on start

--- emacs/.doom.d/+ui.el
+++ emacs/.doom.d/+ui.el
@@ -3,11 +3,9 @@
 (setq doom-theme 'doom-snazzy)
 
 ;; Fonts
-;; 1920x1080, half screen at size 22 gives 80 columns 35 lines
-(setq doom-font (font-spec :family "Source Code Pro" :size 22))
-;; On my 1920x1080, full screen gives about 90 columns 19 lines
+(setq doom-font (font-spec :family "Source Code Pro" :size 18))
 (setq doom-big-font (font-spec :family "Source Code Pro" :size 40))
-(add-hook! 'window-setup-hook #'doom-big-font-mode)
+;; (add-hook! 'window-setup-hook #'doom-big-font-mode)
 ;; Dash highlighting
 (after! dash (dash-enable-font-lock))
 
